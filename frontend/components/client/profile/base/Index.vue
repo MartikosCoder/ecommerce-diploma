58d105f40476261ab2fbe54ac63a858722e698ce
Martin <martbelmoaw@gmail.com>
2024-06-10 20:24:00 +0300

    Добавлены переводы

--- frontend/components/client/profile/base/Index.vue
+++ frontend/components/client/profile/base/Index.vue
@@ -7,7 +7,7 @@ const { data } = useProfile();
 <template>
   <article class="px-2 lg:px-10 w-full flex flex-col gap-5">
     <h1 class="text-2xl lg:text-3xl font-bold">
-      {{ $t("client.profile.base.menu") }}
+      {{ $t("client.profile.personal.menu") }}
     </h1>
     <section
       class="flex flex-col gap-6 lg:flex-row lg:flex-wrap lg:justify-center"
